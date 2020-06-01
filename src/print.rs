c2d8024b3b04e75c6b761c5964db13ee89998f3c
abidurr <42abid@gmail.com>
2020-06-01 01:14:35 -0400

    Placeholders

--- src/print.rs
+++ src/print.rs
@@ -2,14 +2,22 @@ pub fn run() {
     // Print to console
     println!("Hello from print.rs!");
 
+    // Positional and named arguments
+    println!(
+        "{name} is learning {0} just because {name} thought {0} seemed cool",
+        "Rust",
+        name = "Abi"
+    );
+
     // Basic formatting
     println!("{}'s favorite number is {}", "Abi", 11);
 
-    //Positional and named arguments
-    println!(
-        "{name} is learning {0} just because {name} thought {0} seemed cool",
-        "Rust", name = "Abi"
-    );
+    // Placeholder traits
+    println!("Binary: {:b} \nHex: {:x} \nOctal: {:o}\n", 11, 11, 11);
 
+    // Placeholder for debugger trait - tuple
+    println!("{:?}", (11, true, "hi"));
 
+    // Basic Math
+    println!("5 + 6 = {}", 5 + 6);
 }
